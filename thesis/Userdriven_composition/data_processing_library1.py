48477ad89c77f72f0f853e0866a3eaaeb3c3d318
Diganta Biswas <digantaprotik@gmail.com>
2019-03-25 23:16:41 +0600

    weather api added

--- thesis/Userdriven_composition/data_processing_library1.py
+++ thesis/Userdriven_composition/data_processing_library1.py
@@ -3,6 +3,26 @@ import requests
 import json
 
 
+
+
+def weather_info(city):
+	api_key = "bd27419d66c8678613e978ca561ad3f7"
+	url = "http://api.openweathermap.org/data/2.5/forecast?q="+city+"&APPID={}".format(api_key)
+	data = requests.get(url).json()
+
+	#data = data['weather']
+	#print(data['name'])
+	#print(json.dumps(data, indent = 4, sort_keys=True))
+
+	data=(data['list'][0]['main']['temp'])
+	print(data)
+	return data
+
+
+
+
+
+
 def action_motor(motor_status):
 	url = 'http://127.0.0.1:8000/api/actuators/9/'
 
--- thesis/static/js/generators/codegenerator-weather.js
+++ thesis/static/js/generators/codegenerator-weather.js
@@ -1,6 +1,7 @@
-
-Blockly.Python['weather'] = function(Block) {
-    var code = "parseJson()";
-    return [code, Blockly.Python.ORDER_ATOMIC];
-    //return code;
-  }
+Blockly.Python['weather'] = function(block) {
+  var value_to_input = Blockly.Python.valueToCode(block, 'to_input', Blockly.Python.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'weather_info('+value_to_input+')';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_NONE];
+};
